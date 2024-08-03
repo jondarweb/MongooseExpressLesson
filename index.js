db0b3f7ffb81071a1cad4511d69e855b686005d0
John Ward <jondarweb@gmail.com>
2024-08-03 09:56:43 +0100

    basic mongoose and express set up, added seed data

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+
+const Product = require('./models/product');
+
+mongoose.connect('mongodb://127.0.0.1:27017/farmStand', {useNewUrlParser: true}).then(() => 
+{
+  console.log("Mongo Connection Open");
+}).catch(err => {
+console.log("OH NO MONGO CONNECTION ERROR");
+console.log(err);
+})
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get('/dog', (req,res) => {
-  res.send('Woof!')
+app.get('/dog', (req, res) => {
+  res.send('Woof!');
 })
 
 app.listen(3000, () => {
-console.log("App is listening on port 3000)")
+console.log("App is listening on port 5500)")
 })
